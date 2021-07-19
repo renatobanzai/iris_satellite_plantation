cee5861215df9bb0d3b418937d27b7352c690e90
Renato Banzai <renatobanzai@gmail.com>
2021-07-19 04:14:05 -0300

    1st commit

--- app/install.py
+++ app/install.py
@@ -3,5 +3,9 @@ import subprocess
 
 # installing requirements
 subprocess.check_call([sys.executable, '-m', 'pip', 'install','opencv-python'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','pyproj'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','rasterio'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','fiona'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','boto3'])
 
 #creating tables
--- app/process.py
+++ app/process.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
+import boto3
 
 plt.rcParams['figure.figsize'] = [10, 10]
 
@@ -48,3 +49,31 @@ def view_kmeans(img_b04_path, img_b08_path, show=False):
     print("center", center)
     print("result", result)
     return result
+
+def interest_area(image_path, lat, long, square_width):
+    with rs.open(image_path) as rs_img:
+        crs_img = CRS.from_user_input(rs.crs.CRS(rs_img.crs))
+        crs_WSG84 = CRS("WGS84")
+        coord_transformer = Transformer.from_crs(crs_from=crs_WSG84, crs_to=crs_img)
+        res = coord_transformer.transform(long, lat)
+        x = res[0]
+        y = res[1]
+        x_factor = rs_img.width / (rs_img.bounds.right - rs_img.bounds.left)
+        y_factor = rs_img.height / (rs_img.bounds.top - rs_img.bounds.bottom)
+        x_proj = (x - rs_img.bounds.left) * x_factor
+        y_proj = (rs_img.bounds.top - y ) * y_factor
+        # recortando um quadrado usando a latlong indicada como ponto central
+        win_interest = Window(col_off=x_proj - (square_width / 2),
+                              row_off=y_proj - (square_width / 2),
+                              width=square_width,
+                              height=square_width)
+        win_result = rs_img.read(window=win_interest)
+    return win_result
+
+
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+with open('img_test', 'wb') as f:
+    s3.download_fileobj('sentinel-cogs', 'sentinel-s2-l2a-cogs/21/L/YD/2020/10/S2A_21LYD_20201015_0_L2A/TCI.tif', f)
